e961e4a4a9f467c1b0b9eef01978a4ccd6dc37b1
Piotr Domian <domianpiotr111@gmail.com>
2021-04-10 06:00:10 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Zaimplementuj zadanie zgodnie z wytycznymi
 
-let car;
+let car = 
+{
+    name: "Toyota Yaris",
+    price: 50000,
+    led: true,
+    color: "blue",
+};
 
 module.exports = car;
